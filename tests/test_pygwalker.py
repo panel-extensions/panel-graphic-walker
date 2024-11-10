e0e89f603af62cc87fdb4dd0ad4e5b90df1dcd2a
Marc Skov Madsen <masma@orsted.com>
2024-11-10 09:18:05 +0000

    mark xfail

--- tests/test_pygwalker.py
+++ tests/test_pygwalker.py
@@ -4,4 +4,6 @@ from panel_gwalker._gwalker import get_data_parser
 
 
 def test_get_data_parser(data):
+    if str(type(data)) == "<class 'dask_expr._collection.DataFrame'>":
+        pytest.xfail("Dask DataFrame is not supported yet")
     assert get_data_parser(data, [], False, False, {})

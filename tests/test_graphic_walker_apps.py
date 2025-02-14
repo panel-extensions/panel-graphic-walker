be8f31563979bb132e19e0560d025913570a05d9
Philipp Rudiger <prudiger@anaconda.com>
2025-02-14 12:52:52 +0100

    Ensure errors are captured

--- tests/test_graphic_walker_apps.py
+++ tests/test_graphic_walker_apps.py
@@ -1,3 +1,4 @@
+from io import StringIO
 from pathlib import Path
 
 import pytest
@@ -8,4 +9,7 @@ EXAMPLE_APP_PATHS = list(Path("examples").rglob("*.py"))
 
 @pytest.mark.parametrize("path", EXAMPLE_APP_PATHS)
 def test_apps(path):
-    exec_with_return(path.read_text(encoding="utf-8"))
+    stderr = StringIO()
+    exec_with_return(path.read_text(encoding="utf-8"), stderr=stderr)
+    stderr.seek(0)
+    assert stderr.read() == ""

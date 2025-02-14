81c8f70159c6f0cba666f618fbb62dff0432e4b2
Philipp Rudiger <prudiger@anaconda.com>
2025-02-14 12:43:31 +0100

    Fix lint

--- tests/test_graphic_walker_apps.py
+++ tests/test_graphic_walker_apps.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
 import pytest
-
 from panel.io.mime_render import exec_with_return
 
 EXAMPLE_APP_PATHS = list(Path("examples").rglob("*.py"))
@@ -9,4 +8,4 @@ EXAMPLE_APP_PATHS = list(Path("examples").rglob("*.py"))
 
 @pytest.mark.parametrize("path", EXAMPLE_APP_PATHS)
 def test_apps(path):
-    exec_with_return(path.read_text(encoding='utf-8'))
+    exec_with_return(path.read_text(encoding="utf-8"))

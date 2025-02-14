87a7c94d5359305a4194ab25e691eb4586ecb199
Philipp Rudiger <prudiger@anaconda.com>
2025-02-14 12:41:08 +0100

    Improve test execution

--- tests/test_graphic_walker_apps.py
+++ tests/test_graphic_walker_apps.py
@@ -2,19 +2,11 @@ from pathlib import Path
 
 import pytest
 
-EXAMPLE_APP_PATHS = [str(path) for path in Path("examples").rglob("*.py")]
+from panel.io.mime_render import exec_with_return
+
+EXAMPLE_APP_PATHS = list(Path("examples").rglob("*.py"))
 
 
 @pytest.mark.parametrize("path", EXAMPLE_APP_PATHS)
 def test_apps(path):
-    # Quick test that apps can run
-    # Could be improved but I would like to keep theme relatively fast, i.e. without Playwrigth
-    with open(path) as f:
-        code = f.read()
-        env = globals().copy()
-        env["__file__"] = path
-        try:
-            exec(code, env)
-        except:
-            msg = f"Error running {path}"
-            raise Exception(msg)
+    exec_with_return(path.read_text(encoding='utf-8'))
